f183aa7907ca61d9c55ced17b023b68f4b341c3a
상은 <1-blue98@naver.com>
2025-05-10 15:36:31 +0900

    ✨:[lol] metadat 수정

--- apps/lol/src/app/(home)/page.tsx
+++ apps/lol/src/app/(home)/page.tsx
@@ -21,10 +21,18 @@ export const generateMetadata = async ({
 }: IProps): Promise<Metadata> => {
   const { correctAnswers, total, timeMin, timeSec, type } = await searchParams;
 
-  // 기본 메타데이터 값
-  const title = "리그오브레전드(lol) 스킨 퀴즈 결과";
-  const description = `[모드: ${type}] 정답: ${correctAnswers} / ${total} 소요시간: ${timeMin}분 ${timeSec}초`;
+  let title = "리그오브레전드(lol) 스킨 퀴즈";
+  let description = `리그오브레전드(lol) 챔피언 스킨, 아이템, 스킬을 맞춰보세요!\n리그 오브 레전드 챔피언 스킨 지식을 테스트해보세요! 객관식과 주관식 모드를 제공합니다.`;
   const imageUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Bard_35.jpg`;
+
+  // 기본 메타데이터 값
+  if (type === "multiple-choice") {
+    title = "리그오브레전드(lol) 스킨 퀴즈 결과";
+    description = `[모드: ${type}] 정답: ${correctAnswers} / ${total} 소요시간: ${timeMin}분 ${timeSec}초`;
+  } else if (type === "short-answer") {
+    title = "리그오브레전드(lol) 스킨 퀴즈 결과";
+    description = `[모드: ${type}] 정답: ${correctAnswers} / ${total} 소요시간: ${timeMin}분 ${timeSec}초`;
+  }
 
   return {
     title,
